8ac777e20b28e1bd690ace1646d8a60b7dfcb5a2
Somesh7723 <someshkolluru723@gmail.com>
2025-09-27 19:30:56 +0530

    updated env variables and local .env files

--- src/lib/supabseClient.js
+++ src/lib/supabseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Replace these with your Supabase project values
-const SUPABASE_URL = 'https://wztqimzxyfsedozseqxt.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6dHFpbXp4eWZzZWRvenNlcXh0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1Njk2NjYzNSwiZXhwIjoyMDcyNTQyNjM1fQ.AQjnD_miVhCQUsdTpYgJXcQLN5bRwFioDt3CU-M_FCI';
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY =  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
